51bc7dbdb8c7a7208840a0948d88ad76dc5bb15f
cyfcyfcyf1015 <42893811+cyfcyfcyf1015@users.noreply.github.com>
2022-03-29 08:59:43 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "2a47d957-539e-4309-bfb0-3f55cea48cbc",
                         "alterId": 0
                     }
                 ],
